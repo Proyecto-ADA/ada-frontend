a755a616d3968eff714a67abb9b08cc88223a55e
JairEscamilla <jair@kalyptio.com>
2022-11-27 20:10:15 -0600

    Added pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'
+import { NgModule, isDevMode } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
@@ -9,14 +9,14 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth'
 import { environment } from '../environments/environment'
 import { HttpClientModule } from '@angular/common/http'
 import { AngularFireStorageModule } from '@angular/fire/compat/storage'
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { MatIconModule } from '@angular/material/icon'
 import { MatMenuModule } from '@angular/material/menu'
 import { MatButtonModule } from '@angular/material/button'
 import { MatDialogModule } from '@angular/material/dialog'
 import { ReactiveFormsModule } from '@angular/forms'
-import {  MatSnackBarModule } from '@angular/material/snack-bar'
-
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { ServiceWorkerModule } from '@angular/service-worker'
 
 @NgModule({
   declarations: [AppComponent],
@@ -34,7 +34,13 @@ import {  MatSnackBarModule } from '@angular/material/snack-bar'
     MatButtonModule,
     MatDialogModule,
     ReactiveFormsModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: true,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
