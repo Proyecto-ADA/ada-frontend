b8414a94e2aef1a435413a4461c5e4356c3e182b
ErikaDuran1999 <129236981+ErikaDuran1999@users.noreply.github.com>
2023-05-03 10:37:48 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,3 +11,13 @@ if (environment.production) {
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err))
+
+ <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-HPKPDB05FW"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-HPKPDB05FW');
+  </script>
